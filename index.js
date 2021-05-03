ec1a429756c46ba066d753ab6c8b901d1e3c8ae2
letrahu <letrahu99@gmail.com>
2021-05-03 03:36:57 +0000

    feat(debug): add waka time data console

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const octokit = new Octokit({ auth: `token ${githubToken}` });
 
 async function main() {
   const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
+  console.log(stats)
   await updateGist(stats);
 }
 
